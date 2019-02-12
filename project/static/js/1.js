ce314aadff797a0098423a49ef2268c028722a2e
Xanadu12138 <45411494+Xanadu12138@users.noreply.github.com>
2019-02-12 10:27:46 +0800

    1

--- project/static/js/1.js
+++ project/static/js/1.js
@@ -174,3 +174,20 @@ function signin() {
     alert("请输入用户名和密码");
   }
 }
+function menuFixed(id) {
+  var obj = document.getElementById(id);
+  var _getHeight = obj.offsetTop;
+
+  window.onscroll = function() {
+      changePos(_getHeight);
+  }
+}
+function changePos(height) {
+  var obj = document.getElementById('headnavnone');
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  if (scrollTop < height) {
+      obj.style.setProperty('display', 'none');
+  } else {
+      obj.style.setProperty('display', 'block');
+  }
+}
